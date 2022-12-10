c1e62f685cb8c0e990f0ac497cdff17c985c0141
飓枫 <112387102+HurricanMaple@users.noreply.github.com>
2022-12-11 00:45:06 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '飓枫 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u1853293-148e22e754f2ed72f54a01e7',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://fiee.wtdown.top/home'
+    },
+    {
+      text: '博客',
+      url: 'https://fiee.wtdown.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/HurricanMaple/'
+    },
+  ],
+};
